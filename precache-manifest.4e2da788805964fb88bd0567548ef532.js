5da909071c1109f0b3c00755c668628d92f3652b
themos <themos.kost@yahoo.gr>
2020-05-11 13:12:28 +0200

    Updates

---
+++ precache-manifest.4e2da788805964fb88bd0567548ef532.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7218d64169b953b4e5bcf589fd6d165a",
+    "url": "/corona_app/index.html"
+  },
+  {
+    "revision": "be01e2ccbe458e9b7341",
+    "url": "/corona_app/static/css/main.f503a06c.chunk.css"
+  },
+  {
+    "revision": "a94f5283494cf6b1163d",
+    "url": "/corona_app/static/js/2.1c6b39ad.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/corona_app/static/js/2.1c6b39ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be01e2ccbe458e9b7341",
+    "url": "/corona_app/static/js/main.4a80ef65.chunk.js"
+  },
+  {
+    "revision": "79173cfc6e67a8c755d8",
+    "url": "/corona_app/static/js/runtime-main.8cd16d5e.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/corona_app/static/media/image.d7265326.png"
+  }
+]);
